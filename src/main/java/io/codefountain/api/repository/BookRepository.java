858c3106794a0cb33e9c5dc617e84f953aee5f2a
acknowledge <jack_ett@hotmail.com>
2022-09-17 23:26:33 +0200

    Remove old classes

--- src/main/java/io/codefountain/api/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package io.codefountain.api.repository;
-
-import io.codefountain.api.domain.Book;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
-}

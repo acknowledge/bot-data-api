60edbf86b21c10811dcb8493743267905ea312b3
acknowledge <jack_ett@hotmail.com>
2022-09-17 23:30:35 +0200

    Implement basic functionalities in controller and inflate samples in db at startup

---
+++ src/main/java/com/mms/dataapi/api/controller/DialogController.java
@@ -0,0 +1,113 @@
+package com.mms.dataapi.api.controller;
+
+import com.mms.dataapi.api.model.DataPayload;
+import com.mms.dataapi.api.model.Dialog;
+import com.mms.dataapi.api.model.Utterance;
+import com.mms.dataapi.api.repository.DialogRepository;
+import com.mms.dataapi.api.repository.UtteranceRepository;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/")
+public class DialogController {
+    private final DialogRepository dialogRepository;
+    private final UtteranceRepository utteranceRepository;
+
+    public DialogController(DialogRepository dialogRepository, UtteranceRepository utteranceRepository) {
+        this.dialogRepository = dialogRepository;
+        this.utteranceRepository = utteranceRepository;
+    }
+
+    @PostMapping("/data/{customerId}/{dialogId}")
+    ResponseEntity<Utterance> postData(
+            @PathVariable(value="customerId") Long customerId,
+            @PathVariable(value="dialogId") Long dialogId,
+            @RequestBody DataPayload newEntry) {
+        //curl -X POST localhost:8080/data/2/23 -H 'Content-type:application/json' -d '{"text":"Hey hey", "language":"EN"}'
+
+        Optional<Dialog> dialogue = dialogRepository.findById(dialogId);
+        if (dialogue.isPresent()) {
+            if (!dialogue.get().getCustomerId().equals(customerId)) {
+                // This dialogue exists with another customerId
+                return new ResponseEntity("409 CONFLICT - A dialogue already exists for another customer.",
+                        HttpStatus.CONFLICT);
+            }
+            // else: This dialogue already exists, no need to create it
+        } else {
+            // This dialogue does not exist, let's create one
+            Dialog d = new Dialog(dialogId, customerId);
+            dialogue = Optional.of(d);
+            dialogRepository.save(d);
+        }
+        Utterance u = new Utterance(dialogue.get(), newEntry.getText(), newEntry.getLanguage());
+        utteranceRepository.save(u);
+
+        return new ResponseEntity<>(u, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/consents/{dialogId}")
+    ResponseEntity postConsents(
+            @PathVariable(value="dialogId") Long dialogId,
+            @RequestBody @NotNull Boolean consent) {
+
+        Optional<Dialog> dialogue = dialogRepository.findById(dialogId);
+        if (dialogue.isPresent()) {
+            if (!dialogue.get().getConsent()) {
+                // The current consent is false
+                if (consent) {
+                    // New consent is true : we save it
+                    dialogue.get().setConsent(consent);
+                    dialogRepository.save(dialogue.get());
+                    return ResponseEntity.status(HttpStatus.CREATED).body("Consent was given.");
+                } else {
+                    // New consent is false : we remove the data from the DB
+                    dialogRepository.delete(dialogue.get());
+                    return ResponseEntity.status(HttpStatus.CREATED).body("Consent was rejected, data was cleaned.");
+                }
+            }
+            // else: Consent was previously set to true
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No dialogue was found for this dialogId.");
+    }
+
+    @GetMapping("/data")
+    public List<Utterance> getData(
+            @RequestParam("language") Optional<String> language,
+            @RequestParam("customerId") Optional<Long> customerId,
+            @RequestParam(defaultValue = "0") Integer pageNo,
+            @RequestParam(defaultValue = "10") Integer pageSize
+            ){
+
+        // todo: ResponseEntity<List<Dialog>> as return type?
+        // todo: filtering by customerId
+        // todo: filtering by consent=true
+
+        Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by("creationDate").descending());
+
+        Page<Utterance> pagedResult;
+        if (language.isPresent()) {
+            pagedResult = utteranceRepository.findByLanguage(language.get(), paging);
+        } else {
+            pagedResult = utteranceRepository.findAll(paging);
+        }
+
+        if(pagedResult.hasContent()) {
+            return pagedResult.getContent();
+        } else {
+            return new ArrayList<Utterance>();
+        }
+        // todo: do the correct HTTP responses
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
-insert into book(book_Id, book_Title, book_Author, book_Publisher) values(1, 'Java in Action', 'Some Author', 'Pearson');
-insert into book(book_Id, book_Title, book_Author, book_Publisher) values(2, 'Java Primer', 'Some Author', 'Pearson');
-insert into book(book_Id, book_Title, book_Author, book_Publisher) values(3, 'Spring in Action', 'Some Author', 'Pearson');
-insert into book(book_Id, book_Title, book_Author, book_Publisher) values(4, 'JavaScript Primer', 'Some Author', 'Pearson');
-insert into book(book_Id, book_Title, book_Author, book_Publisher) values(5, 'Scala in Action', 'Some Author', 'Pearson');
+insert into dialog(dialog_id, consent, customer_id) values(5, FALSE, 12);
+insert into dialog(dialog_id, consent, customer_id) values(6, FALSE, 12);
+insert into dialog(dialog_id, consent, customer_id) values(7, TRUE, 44);
+insert into utterance(utterance_id, dialog_id, language, text, creation_date) values(1, 5, 'en', 'Hi', '2022-09-17T22:52:57.032234');
+insert into utterance(utterance_id, dialog_id, language, text, creation_date) values(2, 6, 'fr', 'Salut', '2022-09-17T22:56:57.032234');
+insert into utterance(utterance_id, dialog_id, language, text, creation_date) values(3, 7, 'en', 'I want to modify my contract', '2022-09-17T23:15:57.032234');
+insert into utterance(utterance_id, dialog_id, language, text, creation_date) values(4, 7, 'en', 'Yes indeed', '2022-09-17T23:16:57.032234');
+insert into utterance(utterance_id, dialog_id, language, text, creation_date) values(5, 7, 'en', 'Ok, thanks a lot', '2022-09-17T23:17:57.032234');
